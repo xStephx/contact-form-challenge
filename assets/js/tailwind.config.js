7026c51d37f6e6cbdcd07ab42baa5027e1b714eb
xStephx <bojkovskistefan9@gmail.com>
2024-07-26 15:21:22 +0200

    initial setup

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                Green200lighter: 'hsl(148, 38%, 91%)',
+                Green600medium: 'hsl(169, 82%, 27%)',
+                Red: 'hsl(0, 66%, 54%)',
+                White: 'hsl(0, 0%, 100%)',
+                Grey500medium: 'hsl(186, 15%, 59%)',
+                Grey900darker: 'hsl(187, 24%, 22%)',
+            },
+        },
+        fontFamily: {
+            Karla: ['Karla', 'sans-serif'],
+        }
+    },
+    plugins: [],
+}
